d3277b95640bc1c962016cb679ab6f8a81f0676e
Diego-Batista <diegobtistadev@gmail.com>
2023-05-29 12:52:57 -0300

    Button back

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -3,10 +3,10 @@ import { BsArrowLeftShort } from "react-icons/bs";
 
 export function BackButton() {
   return (
-    <Link href="/" className="flex gap-1 items-center justify-center text-xl z-50 text-gray-400 dark:text-[#a87b05]">
-      <BsArrowLeftShort className="w-8 h-8 md:w-10 md:h-10 text-gray-400 dark:text-[#a87b05]" />
-      <span className="hidden md:block">
-      voltar para home
+    <Link href="/" className="group flex items-center justify-center text-xl z-50 text-gray-400 dark:text-[#a87b05]">
+      <BsArrowLeftShort className="group-hover:text-gray-300 w-8 h-8 md:w-9 md:h-9 text-gray-400 dark:text-[#a87b05] p-0" />
+      <span className="hidden md:block font-medium group-hover:text-gray-300 leading-none">
+        Voltar para home
       </span>
     </Link>
   );
